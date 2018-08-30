3fb821f071614f9e1290746d3de4d75ce8f8ec6d
Pedro Paixao <paixaop@gmail.com>
2018-08-30 15:15:40 -0400

    delete clearbt.sh

--- clearbt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Clear the Bluetooth attributes cache on Mac OS X
-# From http://expertsoverflow.com/questions/23549859/clearing-corebluetooth-gatt-cache-without-removing-bond
-
-sudo defaults write /Library/Preferences/com.apple.Bluetooth CoreBluetoothCache -dict
-sudo launchctl unload /System/Library/LaunchDaemons/com.apple.blued.plist
-sudo launchctl load /System/Library/LaunchDaemons/com.apple.blued.plist
-
