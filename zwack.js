8e4f1e41adebb6bd92a6b4cc14980fba08076b5d
Pedro Paixao <paixaop@gmail.com>
2018-08-30 15:05:27 -0400

    refactor most of the power code and add run speed and cadence

--- zwack.js
+++ zwack.js
@@ -1,22 +1,30 @@
-var BluetoothPeripheral = require('./lib/bluetooth-peripheral');
+var ZwackBLE = require('./lib/zwack-ble-sensor');
 const readline = require('readline');
+var convert = require('convert-units');
 
 // default parameters
 var cadence = 90;
 var power = 250;
+var runningCadence = 180;
+var runningSpeed = 10;  // 6:00 minute mile
 var randomness = 5;
 var sensorName = 'Zwack';
 
 var incr = 10;
+var runningIncr = 0.5;
 var stroke_count = 0;
 var notificationInterval = 1000;
-var crankRevolutions = 0;
 var watts = power;
 
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
-var peripheral = new BluetoothPeripheral(sensorName);
+var zwackBLE = new ZwackBLE({ 
+  name: sensorName,
+  modelNumber: 'ZW-101',
+  serialNumber: '1'
+});
+
 process.stdin.on('keypress', (str, key) => {
   if (key.name === 'x' || key.name == 'q' || ( key.ctrl && key.name == 'c' ) ) {
     process.exit(); // eslint-disable-line no-process-exit
@@ -25,9 +33,11 @@ process.stdin.on('keypress', (str, key) => {
   } else {
     if( key.shift ) {
       factor = incr;
+      runFactor = runningIncr;
     }
     else {
       factor = -incr;
+      runFactor = -runningIncr;
     }
 
     switch(key.name) {
@@ -52,6 +62,16 @@ process.stdin.on('keypress', (str, key) => {
         if( randomness < 0 ) {
           randomness = 0;
         }
+      case 's':
+        runningSpeed += runFactor; break;
+        if( runningSpeed < 0 ) {
+          runningSpeed = 0;
+        }
+      case 'd':
+        runningCadence += runFactor; break;
+        if( runningCadence < 0 ) {
+          runningCadence = 0;
+        }
       case 'i':
         incr += Math.abs(factor)/factor; break;
         if( incr < 1 ) {
@@ -65,20 +85,20 @@ process.stdin.on('keypress', (str, key) => {
 });
 
 
-var notifyPower = function() {
+var notifyPowerCSP = function() {
   watts = Math.floor(Math.random() * randomness + power);
 
   try {
-    peripheral.notify({'watts': watts});
+    zwackBLE.notifyCSP({'watts': watts});
   }
   catch( e ) {
     console.error(e);
   }
   
-  setTimeout(notifyPower, notificationInterval);
+  setTimeout(notifyPowerCSP, notificationInterval);
 };
 
-var notifyCadence = function() {
+var notifyCadenceCSP = function() {
   stroke_count += 1;
   if( cadence <= 0) {
     cadence = 0;
@@ -86,32 +106,74 @@ var notifyCadence = function() {
     return;
   }
   try {
-    peripheral.notify({'watts': watts, 'rev_count': stroke_count });
+    zwackBLE.notifyCSP({'watts': watts, 'rev_count': stroke_count });
   }
   catch( e ) {
     console.error(e);
   }
   
-  setTimeout(notifyCadence, 60 * 1000/(Math.random() * randomness + cadence));
+  setTimeout(notifyCadenceCSP, 60 * 1000/(Math.random() * randomness + cadence));
+};
+
+
+var notifyRSC = function() {
+  watts = Math.floor(Math.random() * randomness + power);
+
+  try {
+    zwackBLE.notifyRSC({
+      'speed': toMs(runningSpeed),
+      'cadence': runningCadence
+    });
+  }
+  catch( e ) {
+    console.error(e);
+  }
+  
+  setTimeout(notifyRSC, notificationInterval);
 };
 
 function listParams() {
   console.log(`\nBLE Sensor parameters:`);
-  console.log(`Cadence: ${cadence}`);
-  console.log(`Power: ${power}`);
-  console.log(`Randomness: ${randomness}`);
+  console.log(`\nCycling:`)
+  console.log(`    Cadence: ${cadence} RPM`);
+  console.log(`    Power: ${power} W`);
+
+  console.log('\nRunning:');
+
+  console.log(`    Speed: ${runningSpeed} m/h, Pace: ${speedToPace(runningSpeed)} min/mi`);
+  console.log(`    Cadence: ${Math.floor(runningCadence)} steps/min`);
+
+  console.log(`\nRandomness: ${randomness}`);
   console.log(`Increment: ${incr}`);
   console.log('\n');
 }
 
 function listKeys() {
   console.log(`\nList of Available Keys`);
-  console.log('c/C - Decrease/Increase cadence');
-  console.log('p/P - Decrease/Increase power');
+  console.log('c/C - Decrease/Increase cycling cadence');
+  console.log('p/P - Decrease/Increase cycling power');
+
+  console.log('s/S - Decrease/Increase running speed');
+  console.log('d/D - Decrease/Increase running cadence');
+
   console.log('\nr/R - Decrease/Increase parameter variability');
   console.log('i/I - Decrease/Increase parameter increment');
   console.log('x/q - Exit');
   console.log();
+}
+
+function speedToPace(speed) {
+  if( speed === 0 ) {
+    return '00:00';
+  }
+  let t = 60 / speed;
+  let minutes = Math.floor(t);
+  let seconds = Math.floor((t - minutes) * 60);
+  return minutes.toString().padStart(2,'0') + ':' + seconds.toString().padStart(2,'0');
+}
+
+function toMs(speed) {
+  return (speed * 1.60934) / 3.6;
 }
 
 // Main
@@ -119,6 +181,6 @@ console.log(`[ZWack] Faking test data for sensor: ${sensorName}`);
 
 listKeys();
 listParams();
-notifyPower();
-notifyCadence();
-
+notifyPowerCSP();
+notifyCadenceCSP();
+notifyRSC();
